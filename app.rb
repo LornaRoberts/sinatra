4316f152977dbc56d884fd5778be12f47d41945e
Lorna Roberts <lornaroberts99@gmail.com>
2020-10-27 15:07:23 +0000

    added cat names

--- app.rb
+++ app.rb
@@ -4,10 +4,18 @@ get '/' do
   "Hello"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @middlename = params[:middlename]
+  @lastname = params[:lastname]
+  erb(:index)
+end
 
 
 set :session_secret, 'super secret'
